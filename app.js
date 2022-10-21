5330e9d1800f321f7b7dc53b635c27afc2f8566c
ankit-pn <miankitsingh@gmail.com>
2022-10-21 21:48:51 +0530

    modified delete

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.post("/deleteListItems", async (request, response) => {
     const available_quantity = request.body.available_quantity;
     const require_quantity = request.body.require_quantity;
     const vendor_address = request.body.vendor_address;
-    await Date.deleteOne({
+    await items.deleteOne({
         userId: userId,
         itemId: itemId,
         price:price,
